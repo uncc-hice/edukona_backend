72ee16357f56a785a8834d0334335c5d7f00467d
“Aryan <aaladar@uncc.edu>
2024-07-22 21:07:29 -0400

    return a user count along with the setting for the frontend to disply

--- api/consumers.py
+++ api/consumers.py
@@ -30,7 +30,13 @@ class QuizSessionInstructorConsumer(AsyncWebsocketConsumer):
         await self.send(text_data=json.dumps({
             'type': 'settings',
             'settings': settings
+            'user_count': await self.fetch_user_count()
         }))
+
+    @database_sync_to_async
+    def fetch_user_count(self):
+        session = QuizSession.objects.get(code=self.code)
+        return session.students.count()
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(self.group_name, self.channel_name)
