74be1e0426be68aabf3e15ebbb056178925c5812
amhajja <amhajja@gmail.com>
2024-09-13 11:00:47 -0400

    Refactor quiz_views imports
    Removed unused imports in quiz_views.py to enhance code clarity and maintainability. This simplifies the file, ensuring only essential modules are included.

--- api/views/quiz_views.py
+++ api/views/quiz_views.py
@@ -1,13 +1,6 @@
-from django.conf import settings
-from django.db import transaction
-
-from rest_framework.response import Response
 from rest_framework.views import APIView
 from api.models import *
 from django.shortcuts import get_object_or_404
-from django.contrib.auth.models import User
-from rest_framework.permissions import AllowAny
-from rest_framework import status
 from django.http import JsonResponse
 
 
