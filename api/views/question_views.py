fb15df9eb6f1c0a9c1802e9aa3c399893e2eb9c5
amhajja <amhajja@gmail.com>
2024-09-13 10:56:50 -0400

    Refactor question_views imports
    Cleaned up unused imports in question_views.py to enhance code clarity and maintainability. This reduces the clutter and focuses on only the necessary dependencies.

--- api/views/question_views.py
+++ api/views/question_views.py
@@ -1,19 +1,12 @@
-from django.conf import settings
 from django.db import transaction
 
-from rest_framework.response import Response
 from rest_framework.views import APIView
 from api.models import *
 from django.shortcuts import get_object_or_404
-from django.contrib.auth.models import User
-from rest_framework.permissions import AllowAny
 from rest_framework import status
 from api.serializers import (
     QuestionMultipleChoiceSerializer,
 )
-from rest_framework import serializers
-from drf_spectacular.utils import extend_schema
-from drf_spectacular.types import OpenApiTypes
 from django.http import JsonResponse
 
 
