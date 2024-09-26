e4fc8798cf6e17277e1d20d6494e8711608fd48f
amhajja <amhajja@gmail.com>
2024-09-26 18:36:35 -0400

    Add Google login functionality
    Implemented Google login by adding Google token verification and user creation or retrieval based

--- api/views/user_views.py
+++ api/views/user_views.py
@@ -12,6 +12,9 @@ from rest_framework.permissions import AllowAny
 from rest_framework import status
 from rest_framework import serializers
 
+from google.oauth2 import id_token
+from google.auth.transport import requests as google_requests
+
 from api.models import (
     Instructor,
     UserResponse,
@@ -24,6 +27,8 @@ from api.serializers import (
     InstructorRecordingsSerializer,
     UpdateTranscriptSerializer,
     GetTranscriptResponseSerializer,
+    GoogleLoginResponseSerializer,
+    GoogleLoginRequestSerializer,
 )
 from drf_spectacular.utils import extend_schema
 from drf_spectacular.types import OpenApiTypes
@@ -437,3 +442,50 @@ class GetTranscriptView(APIView):
             )
 
         return JsonResponse({"transcript": recording.transcript}, status=status.HTTP_200_OK)
+
+
+class GoogleLogin(APIView):
+    permission_classes = [AllowAny]
+
+    @extend_schema(
+        request=GoogleLoginRequestSerializer,
+        responses={
+            200: GoogleLoginResponseSerializer,
+            400: OpenApiTypes.OBJECT,
+        },
+    )
+    def post(self, request):
+        token = request.data.get("token")
+
+        if not token:
+            return Response({"message": "Token not provided"}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            # Verify the Google token
+            id_info = id_token.verify_oauth2_token(
+                token, google_requests.Request(), settings.GOOGLE_CLIENT_ID
+            )
+
+            # Get the user info from the token
+            email = id_info.get("email")
+            name = id_info.get("name")
+
+            # Create or get the user based on the email
+            user, created = User.objects.get_or_create(
+                email=email, defaults={"username": email, "first_name": name}
+            )
+
+            # Generate a DRF token for the user
+            token, _ = Token.objects.get_or_create(user=user)
+            result = {
+                "token": token.key,
+                "user": {
+                    "email": user.email,
+                    "first_name": user.first_name,
+                },
+            }
+
+            return Response(result, status=status.HTTP_200_OK)
+
+        except ValueError as e:
+            return Response({"message": "Invalid token"}, status=status.HTTP_400_BAD_REQUEST)
