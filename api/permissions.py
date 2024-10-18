d335954eb1c24f0006ffeef30680e5de850d1c52
austinfhunter <austinfhunter@gmail.com>
2024-10-18 16:23:50 -0400

    Revert "add IsQuizOwner custom permissions"
    This reverts commit 8b060e8232a849d2d518837f69b953cec38b01c9.

--- api/permissions.py
+++ api/permissions.py
@@ -8,12 +8,3 @@ class AllowInstructor(permissions.BasePermission):
             return False
 
         return Instructor.objects.filter(user=request.user).exists()
-
-class IsQuizOwner(permissions.BasePermission):
-    def has_permission(self, request, view):
-        if not request.user or not request.user.is_authenticated:
-            return False
-        return Instructor.objects.filter(user=request.user).exists()
-
-    def has_object_permission(self, request, view, obj):
-        return obj.instructor == request.user.instructor
