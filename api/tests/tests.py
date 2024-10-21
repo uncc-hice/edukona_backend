11d457528a22b7205c847bfc3aaecf598f48efc4
amhajja <amhajja@gmail.com>
2024-10-20 23:34:31 -0400

    Remove unnecessary print statements in tests
    Removed redundant print statements from the test_get_quizzes method in tests.py. This cleanup improves the readability and maintainability of test code by eliminating clutter.

--- api/tests/tests.py
+++ api/tests/tests.py
@@ -197,23 +197,6 @@ class CreateQuizViewTest(BaseTest):
 
 class InstructorQuizzesViewTest(BaseTest):
     def test_get_quizzes(self):
-        print("Testing changes here..")
-        print("Testing changes here..")
-        print("Testing changes here..")
-        print("Testing changes here..")
-        print("Testing changes here..")
-        print("Testing changes here..")
-        print("Testing changes here..")
-        print("Testing changes here..")
-        print("Testing changes here..")
-        print("Testing changes here..")
-        print("Testing changes here..")
-        print("Testing changes here..")
-        print("Testing changes here..")
-        print("Testing changes here..")
-        print("Testing changes here..")
-        print("Testing changes here..")
-        print("Testing changes here..")
         url = reverse("instructor-quizzes")
         instructor_response = self.client_instructor.get(url)
 
