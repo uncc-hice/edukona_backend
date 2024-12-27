3d2664e41c2cefbeba5d510caca3d5d157e70caa
austinfhunter <austinfhunter@gmail.com>
2024-12-27 01:29:06 -0500

    add course and published fields to models and create tests

--- api/tests/test_course.py
+++ api/tests/test_course.py
@@ -1,8 +1,8 @@
 from django.test import TestCase
-from api.models import Course, Instructor, User
+from api.models import Course, Instructor, User, Quiz, LectureSummary, InstructorRecordings
 
 
-class CourseModelTests(TestCase):
+class BaseCourseTest(TestCase):
     def setUp(self):
         # Create Instructor User
         self.user = User.objects.create_user(
@@ -24,6 +24,8 @@ class CourseModelTests(TestCase):
         self.course.code = self.course.generate_code()
         self.course.save()
 
+
+class CourseModelTests(BaseCourseTest):
     def test_generate_course_code(self):
         self.assertEqual(self.course.code, "TInstructorExample-Course")
 
@@ -32,3 +34,54 @@ class CourseModelTests(TestCase):
         new_course_code = self.course.generate_code()
         pattern = r".-[0-9a-f]{2}"
         self.assertRegex(new_course_code, pattern)
+
+    def test_create_course_recording(self):
+        recording1 = InstructorRecordings.objects.create(
+            instructor=self.instructor, title="test 1", course=self.course, published=True
+        )
+        recording2 = InstructorRecordings.objects.create(
+            instructor=self.instructor, title="test 2", course=self.course
+        )
+        recording1.save()
+        recording2.save()
+        retrieved1 = InstructorRecordings.objects.filter(id=recording1.id).first()
+        retrieved2 = InstructorRecordings.objects.filter(id=recording2.id).first()
+        self.assertEqual(retrieved1.course, self.course)
+        self.assertEqual(retrieved2.course, self.course)
+        self.assertEqual(retrieved1.published, True)
+        self.assertEqual(retrieved2.published, False)
+        self.assertEqual(InstructorRecordings.objects.filter(course=self.course).count(), 2)
+
+    def test_create_course_lec_summary(self):
+        recording = InstructorRecordings.objects.create(
+            instructor=self.instructor, title="test", course=self.course
+        )
+        recording.save()
+        summary1 = LectureSummary.objects.create(
+            recording=recording, course=self.course, published=True
+        )
+        summary2 = LectureSummary.objects.create(recording=recording, course=self.course)
+        summary1.save()
+        summary2.save()
+        retrieved1 = LectureSummary.objects.filter(id=summary1.id).first()
+        retrieved2 = LectureSummary.objects.filter(id=summary2.id).first()
+        self.assertEqual(retrieved1.course, self.course)
+        self.assertEqual(retrieved2.course, self.course)
+        self.assertEqual(retrieved1.published, True)
+        self.assertEqual(retrieved2.published, False)
+        self.assertEqual(LectureSummary.objects.filter(course=self.course).count(), 2)
+
+    def test_create_course_quiz(self):
+        quiz1 = Quiz.objects.create(
+            instructor=self.instructor, title="test", course=self.course, published=True
+        )
+        quiz2 = Quiz.objects.create(instructor=self.instructor, title="test", course=self.course)
+        quiz1.save()
+        quiz2.save()
+        retrieved1 = Quiz.objects.filter(id=quiz1.id).first()
+        retrieved2 = Quiz.objects.filter(id=quiz2.id).first()
+        self.assertEqual(retrieved1.course, self.course)
+        self.assertEqual(retrieved2.course, self.course)
+        self.assertEqual(retrieved1.published, True)
+        self.assertEqual(retrieved2.published, False)
+        self.assertEqual(Quiz.objects.filter(course=self.course).count(), 2)
