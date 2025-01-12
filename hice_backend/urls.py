a56cdada5dc6a5e35e9cdb5214ebcbdeb0547d18
austinfhunter <austinfhunter@gmail.com>
2025-01-12 15:28:36 -0500

    add forward slash to endpoint uri

--- hice_backend/urls.py
+++ hice_backend/urls.py
@@ -213,7 +213,7 @@ urlpatterns = [
         GetRecordingsByCourse.as_view(),
         name="get-course-recordings",
     ),
-    path("instructor/get-courses", GetCoursesByInstructor.as_view(), name="get-instructor-courses"),
+    path("instructor/get-courses/", GetCoursesByInstructor.as_view(), name="get-instructor-courses"),
     path(
         "token/verify/",
         TokenVerificationView.as_view(),
