568e3e7b675c22d2ef71467a1f8352b3cac3c698
austinfhunter <austinfhunter@gmail.com>
2025-01-12 23:59:27 -0500

    add endpoint to fetch summaries by course

--- api/views/course_views.py
+++ api/views/course_views.py
@@ -1,8 +1,12 @@
 from rest_framework.views import APIView
-from api.models import InstructorRecordings, Course
+from api.models import InstructorRecordings, Course, LectureSummary
 from rest_framework import status
 from rest_framework.response import Response
-from api.serializers import InstructorRecordingsSerializer, CourseSerializer
+from api.serializers import (
+    InstructorRecordingsSerializer,
+    CourseSerializer,
+    LectureSummarySerializer,
+)
 from drf_spectacular.utils import extend_schema, OpenApiResponse
 
 from api.permissions import AllowInstructor, IsCourseOwner
@@ -43,3 +47,23 @@ class GetCoursesByInstructor(APIView):
     def get(self, request):
         courses = Course.objects.filter(instructor=request.user.instructor).order_by("-created_at")
         return Response(CourseSerializer(courses, many=True).data, status=status.HTTP_200_OK)
+
+
+@extend_schema(tags=["Instructor Course Management"])
+class GetSummariesByCourse(APIView):
+    permission_classes = [AllowInstructor & IsCourseOwner]
+
+    @extend_schema(
+        responses={
+            200: LectureSummarySerializer(many=True),
+            400: OpenApiResponse(description="Bad Request"),
+            401: OpenApiResponse(description="Unauthorized"),
+            403: OpenApiResponse(description="Forbidden"),
+            404: OpenApiResponse(description="Course not found"),
+        }
+    )
+    def get(self, request, course_id):
+        summaries = LectureSummary.objects.filter(course=course_id).order_by("-created_at")
+        return Response(
+            LectureSummarySerializer(summaries, many=True).data, status=status.HTTP_200_OK
+        )
