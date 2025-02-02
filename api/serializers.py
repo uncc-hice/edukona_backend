8906e6b3b64ba899719331fc400b87fe8b02c90b
BruhLemma-Yadecha <98387741+BruhLemma-Yadecha@users.noreply.github.com>
2025-02-02 18:07:18 -0500

    feat: add serializers for scoring quizzes and retrieving scores

--- api/serializers.py
+++ api/serializers.py
@@ -410,3 +410,17 @@ class LoginErrorResponseSerializer(serializers.Serializer):
 
 class LogoutSerializer(serializers.Serializer):
     refresh = serializers.CharField()
+
+
+class ScoreQuizRequestSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+
+
+class ScoreQuizResponseSerializer(serializers.Serializer):
+    message = serializers.CharField()
+
+
+class GetScoreResponseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = QuizSessionStudent
+        fields = ["score"]
